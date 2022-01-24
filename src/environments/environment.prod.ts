c13040e2f9bbca8f6f7918ed1733d11b049c6887
franzmueller <franzmueller@users.noreply.github.com>
2022-01-24 08:45:02 +0100

    fix keycloak url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   keycloak: {
-    url: 'KEYCLOAK_URL'+'/auth',
+    url: 'KEYCLOAK_URL',
     realm: 'KEYCLOAK_REALM',
     clientId: 'KEYCLOAK_CLIENT_ID'
   }
